0595b229aa9415c22063cd2d0b696249cd3bc745
PengleZhang <guyan364@gmail.com>
2025-11-27 23:06:21 +0800

    greedy search

--- nanovllm/layers/sampler.py
+++ nanovllm/layers/sampler.py
@@ -9,7 +9,4 @@ class Sampler(nn.Module):
 
     @torch.compile
     def forward(self, logits: torch.Tensor, temperatures: torch.Tensor):
-        logits = logits.float().div_(temperatures.unsqueeze(dim=1))
-        probs = torch.softmax(logits, dim=-1)
-        sample_tokens = probs.div_(torch.empty_like(probs).exponential_(1).clamp_min_(1e-10)).argmax(dim=-1)
-        return sample_tokens
+        return logits.argmax(dim=-1)
--- nanovllm/sampling_params.py
+++ nanovllm/sampling_params.py
@@ -7,5 +7,3 @@ class SamplingParams:
     max_tokens: int = 64
     ignore_eos: bool = False
 
-    def __post_init__(self):
-        assert self.temperature > 1e-10, "greedy sampling is not permitted"
